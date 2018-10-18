da9c621120736079ba7cc3843390519248aafa87
David Cornelius T <davidcornelius15@gmail.com>
2018-10-18 11:12:53 +0700

    Initial Commit

---
+++ TestingApp/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  TestingApp
+//
+//  Created by David Cornelius on 18/10/18.
+//  Copyright © 2018 David Cornelius. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
